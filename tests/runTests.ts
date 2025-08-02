bc6d741de5f95652b1ccb3fd21c1a461c6fea846
Tetsu2 <148081847+waseda19@users.noreply.github.com>
2025-08-02 16:09:53 +0900

    Expand question set and add data tests

--- tests/runTests.ts
+++ tests/runTests.ts
@@ -1,9 +1,11 @@
 import { run as runShuffle } from './shuffleQuestions.test';
 import { run as runCategory } from './categoryFilter.test';
 import { run as runEval } from './evaluateAnswer.test';
+import { run as runData } from './questionsData.test';
 
 runShuffle();
 runCategory();
 runEval();
+runData();
 
 console.log('All tests passed');
